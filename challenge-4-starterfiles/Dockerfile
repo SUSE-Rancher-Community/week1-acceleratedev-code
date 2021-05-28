a9753a432526635827b04dd1ed63096051b5ae8f
Tim Irnich <tim.irnich@suse.com>
2021-05-28 09:16:51 +0200

    adding challenge 4 starter files

---
+++ challenge-4-starterfiles/Dockerfile
@@ -0,0 +1,19 @@
+FROM redis:6.2
+
+RUN apt-get clean \
+    && apt-get -y update
+
+RUN apt-get -y install \
+    python3
+
+WORKDIR /app
+
+COPY requirements.txt /app/requirements.txt
+RUN pip install -r requirements.txt
+
+COPY app.py /app/app.py
+COPY templates /app/templates
+
+EXPOSE 8000
+
+CMD [ "python3", "app.py" ]
---
+++ challenge-4-starterfiles/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, render_template, request, jsonify
+
+import os
+
+import redis
+
+app = Flask(__name__)
+
+redisdb = redis.Redis(host='redis', port=6379, db=0)
+
+@app.route('/')
+def index():
+	shoppinglist = redisdb.lrange("list", 0, -1)
+	return render_template('index.html', shoppinglist_formatted=list(shoppinglist))
+
+@app.route('/addentry', methods=['POST'])
+def add_value():
+	value = request.form['value']
+	print("Received entry " + value + ", adding to shopping list...")
+	redisdb.rpush("list", value)
+	return redirect("/", code=302)
+
+@app.route('/removeentry', methods=['POST'])
+def remove_value():
+	value = request.form['value']
+	print("Received entry " + value + ", removing from shopping list...")
+	redisdb.lrem("list", 0, value)
+	return redirect("/", code=302)
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=8000)
